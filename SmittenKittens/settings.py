1f59978ba6f1b469fd8af10ccc148cfee66259c2
MSalvagni <megansalvagni@gmail.com>
2026-07-03 19:55:18 -0400

    Add login/logout and navbar

--- SmittenKittens/settings.py
+++ SmittenKittens/settings.py
@@ -122,3 +122,6 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = "/media/"  # uploaded user files (images)
 MEDIA_ROOT = BASE_DIR / "media"  # where uploaded images are stored
+
+LOGIN_REDIRECT_URL = "/"  # Redirects after login -> home
+LOGOUT_REDIRECT_URL = "/"  # Redirects after logout -> home
