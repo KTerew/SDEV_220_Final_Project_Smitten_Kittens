e7173e08e7943118ef6b3524feef2084305cafb7
MSalvagni <megansalvagni@gmail.com>
2026-07-02 23:27:04 -0400

    Add kitten listings with images and homepage display

--- kittens/views.py
+++ kittens/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Listing
+
 
 def home(request):
-    return render(request, "kittens/home.html", {})
+    listings = Listing.objects.filter(kittens__isnull=False).distinct()  # Homepage only shows listings with kittens in them
+    return render(request, "kittens/home.html", {
+        "listings": listings}  # Pass listings from view to template for display
+    )
